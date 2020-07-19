704adb23307e537fbb4c6bfd464853e3d4d0d705
Matt Corallo <git@bluematt.me>
2020-07-19 17:15:35 -0400

    Decrease thread count somewhat

--- src/main.rs
+++ src/main.rs
@@ -422,7 +422,7 @@ fn main() {
 	unsafe { REQUEST_BLOCK = Some(Box::new(Mutex::new(Arc::new((0, genesis_block(Network::Bitcoin).bitcoin_hash(), genesis_block(Network::Bitcoin)))))) };
 
 	let trt = tokio::runtime::Builder::new()
-		.blocking_threads(2).core_threads(num_cpus::get().max(1) * 2)
+		.blocking_threads(2).core_threads(num_cpus::get().max(1) + 1)
 		.build().unwrap();
 
 	let _ = trt.block_on_all(future::lazy(|| {
